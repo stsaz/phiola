961c4ed6f1a38087c115b50910750e1fee0e4ce5
Simon Zolin <4729655+stsaz@users.noreply.github.com>
2023-12-23 08:00:53 +0300

    v2.0-beta12

--- src/phiola.h
+++ src/phiola.h
@@ -5,7 +5,7 @@
 #include <ffsys/timerqueue.h>
 #include <ffbase/string.h>
 
-#define PHI_VERSION  20011
+#define PHI_VERSION  20012
 
 /** Inter-module compatibility version.
 It must be updated when incompatible changes are made to this file,
