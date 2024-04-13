1455783b030eb0fc3b4754772be002e9d0e34e0e
Simon Zolin <4729655+stsaz@users.noreply.github.com>
2024-04-13 11:13:22 +0300

    2.0-rc22

--- src/core/core.c
+++ src/core/core.c
@@ -33,7 +33,7 @@ do { \
 } while (0)
 
 #ifndef PHI_VERSION_STR
-	#define PHI_VERSION_STR  "2.0-rc21"
+	#define PHI_VERSION_STR  "2.0-rc22"
 #endif
 
 #include <core/worker.h>
--- src/phiola.h
+++ src/phiola.h
@@ -5,7 +5,7 @@
 #include <ffsys/timerqueue.h>
 #include <ffbase/string.h>
 
-#define PHI_VERSION  20021
+#define PHI_VERSION  20022
 
 /** Inter-module compatibility version.
 It must be updated when incompatible changes are made to this file,
