f3569add8deb76e6e4820743ce61e7424a417e33
Simon Zolin <4729655+stsaz@users.noreply.github.com>
2025-01-11 11:27:01 +0300

    2.3-beta5

--- src/phiola.h
+++ src/phiola.h
@@ -6,13 +6,13 @@
 #include <ffbase/string.h>
 #include <ffbase/time.h>
 
-#define PHI_VERSION  20304
+#define PHI_VERSION  20305
 
 /** Inter-module compatibility version.
 It must be updated when incompatible changes are made to this file,
  then all modules must be rebuilt.
 The core will refuse to load modules built for any other core version. */
-#define PHI_VERSION_CORE  20304
+#define PHI_VERSION_CORE  20305
 
 typedef long long int64;
 typedef unsigned long long uint64;
