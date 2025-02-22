bf9f85b1c6be404386ba160a5ccc669c93ae9db5
Simon Zolin <4729655+stsaz@users.noreply.github.com>
2025-02-22 09:25:01 +0300

    2.3.11

--- src/phiola.h
+++ src/phiola.h
@@ -6,7 +6,7 @@
 #include <ffbase/string.h>
 #include <ffbase/time.h>
 
-#define PHI_VERSION  20310
+#define PHI_VERSION  20311
 
 /** Inter-module compatibility version.
 It must be updated when incompatible changes are made to this file,
